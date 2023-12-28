8a413bea8a0a92bfa42dcf8ad36de9bfcc86b984
Denis Mironov <den.hotshot@gmail.com>
2023-12-28 16:02:36 +0100

    Create new order factory, add changes to merchant factory

--- spec/factories/disbursement_factory.rb
+++ spec/factories/disbursement_factory.rb
@@ -2,13 +2,14 @@
 
 FactoryBot.define do
   factory :disbursement do
-    reference { reference_name }
-    total_fee { 5.55 }
-    total_net_amount { 555.0 }
+    reference { uniq_reference_name }
+    gross_amount { 100 }
+    total_fee { 0.95 }
+    net_amount { 99.05 }
   end
 end
 
-def reference_name
+def uniq_reference_name
   name = Faker::Company.unique.name.downcase.parameterize.underscore
   "#{name}_#{Date.current.strftime('%d_%m_%Y')}"
 end
---
+++ spec/factories/order_factory.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :order do
+    reference { Faker::Company.unique.name.downcase.parameterize.underscore }
+    amount { 100 }
+    fee { 0.95 }
+    net_amount { 99.05 }
+    disbursed { false }
+
+    trait :disbursed do
+      disbursed { true }
+    end
+
+    trait :not_disbursed do
+      disbursed { false }
+    end
+  end
+end
