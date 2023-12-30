bb92c73fc6b282efa37ebb417fc0fcc1f29e0754
Denis Mironov <den.hotshot@gmail.com>
2023-12-30 18:30:00 +0100

    Create tests for daily / weekly disbursements calculation workers

---
+++ spec/workers/calculate_weekly_disbursements_worker_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../support/shared_examples/workers_performing'
+
+RSpec.describe CalculateWeeklyDisbursementsWorker do
+  subject(:perform_worker) { described_class.new.perform }
+
+  let(:merchant_1) { create(:merchant, :disbursed_weekly) }
+  let(:merchant_2) { create(:merchant, :disbursed_weekly) }
+
+  let!(:order_1) { create(:order, :undisbursed, merchant: merchant_1, created_at: 3.days.ago) }
+  let!(:order_2) { create(:order, :undisbursed, merchant: merchant_1, created_at: 5.days.ago) }
+  let!(:order_3) { create(:order, :undisbursed, merchant: merchant_1, created_at: 4.days.ago) }
+
+  before do
+    create(:order, :undisbursed, merchant: merchant_1, created_at: Date.current)
+    create(:order, :undisbursed, merchant: merchant_1, created_at: 9.days.ago)
+    create(:order, :disbursed, merchant: merchant_1, created_at: 2.days.ago)
+    create(:order, :disbursed, merchant: merchant_1, created_at: 5.days.ago)
+
+    create(:order, :undisbursed, merchant: merchant_2, created_at: 8.days.ago)
+    create(:order, :undisbursed, merchant: merchant_2, created_at: Date.current)
+  end
+
+  describe '#perform' do
+    let(:service_instance) { instance_double(CreateDisbursementService, call: disbursement) }
+    let(:disbursement) { create(:disbursement) }
+    let(:reference_date) { Date.current }
+    let(:orders) { Order.where(id: [order_1.id, order_2.id, order_3.id]) }
+    let(:values) { orders.pluck('SUM(amount)', 'SUM(fee)', 'SUM(net_amount)').flatten }
+    let(:named_values) do
+      {
+        gross_amount: values[0],
+        total_fee: values[1],
+        net_amount: values[2]
+      }
+    end
+
+    context 'when merchant is dibursed successfully' do
+      before do
+        allow(CreateDisbursementService).to receive(:new)
+          .with(merchant_1, reference_date, named_values)
+          .and_return(service_instance)
+      end
+
+      include_examples 'creates an instance of CreateDisbursementService'
+      include_examples 'calls CreateDisbursementService instance'
+      include_examples 'sets orders disbursed value to true'
+      include_examples 'sets disbursement_id for orders'
+    end
+
+    context 'when merchant is disbursed with errors' do
+      let(:error_message) { /Reference has already been taken/ }
+
+      before do
+        allow(Rails.logger).to receive(:error)
+        allow(CreateDisbursementService).to receive(:new)
+          .with(merchant_1, reference_date, named_values)
+          .and_return(service_instance)
+        allow(service_instance).to receive(:call).and_raise(ActiveModel::StrictValidationFailed, error_message)
+      end
+
+      include_examples 'creates an instance of CreateDisbursementService'
+      include_examples 'calls CreateDisbursementService instance'
+      include_examples 'doesn\'t set orders disbursed value to true'
+      include_examples 'doesn\'t set disbursement_id for orders'
+
+      it 'writes error log' do
+        perform_worker
+
+        expect(Rails.logger).to have_received(:error).with(error_message)
+      end
+    end
+  end
+end
